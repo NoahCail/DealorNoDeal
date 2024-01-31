53f39f3e894349aa0db10bcd7536778923869f7f
Noah Cail <noahcail96@gmail.com>
2024-01-31 15:50:01 -0500

    Added comments and removed redundant code with functions.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random as r
 
+#TODO: Add in testing by using try syntax
+
+
+
 #List of all the brief case numbers
 case_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
@@ -11,15 +15,53 @@ case_values = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5
 personal_case_number = 0
 personal_case_value = 0
 
-
+#
 countdown = 6
 
+#Function that reports the current remaining cases
+def current_cases(case_numbers, case_values):
+    
+    print("Remaining Cases:\n", case_numbers, "\nRemaining Values:\n", case_values)
+
+#Function for having the player select their personal case
 def personal_case_process(case_number):
+
     selection = int(input("Which of these cases do you think has the million dollars in it?: "))
     if selection in case_numbers:
         case_number += selection
         case_numbers.remove(selection)
 
+#Funtion for having the player select a case to eliminate from the board
+def round_process():
+
+    current_cases(case_numbers, case_values)
+    selection = int(input("\nWhat case would you like to select?: "))
+    if selection in case_numbers:
+        case_numbers.remove(selection)
+        value = r.choice(case_values)
+        print("\nCase #" + str(selection) + "'s value was " + str(value) + "!\n")
+        case_values.remove(value)
+    else:
+        print("\nPlease choose a case number from the list of case numbers shown below:\n")
+        round_process()
+
+#Function that generates an offer from the banker for the player to review and decide on
+def banker_process(values):
+
+    offer = int(sum(values) / len(values))
+    current_cases(case_numbers, case_values)
+    print("\nAt this time the banker is willing to offer you $" + str(offer) + " to walk away!\nWould you like to take this deal?\n")
+    response = input("Y or N: ").upper()
+    if response == "Y":
+        print("\nCongrats you have won $" + str(offer), "\nThanks for playing!")
+    elif response == "N":
+        print("\nAlright then the game continues!\n")
+    else:
+        print("\nPlease only answer with a Y or N")
+        banker_process(case_values)
+    return response
+
+#Function that starts the game and progresses it through as the player makes selections
 def game_play(values, countdown):
     
     for i in range(countdown):
@@ -52,41 +94,7 @@ def game_play(values, countdown):
     #         countdown = 6
     #         personal_case_process()
     #         game_play()
-
-def round_process():
-    print("Remaining Cases:\n", case_numbers)
-    print("\nRemaining Values:\n", case_values)
-    selection = int(input("\nWhat case would you like to select?: "))
-    if selection in case_numbers:
-        case_numbers.remove(selection)
-        value = r.choice(case_values)
-        print("\nCase #" + str(selection) + "'s value was " + str(value) + "!\n")
-        case_values.remove(value)
-    else:
-        print("\nPlease choose a case number from the list of case numbers shown below:\n")
-        round_process()
-
-def banker_process(values):
-    total = 0
-    for i in values:
-        total += i
-    offer = int(total / len(values))
-    print("Remaining Cases:\n", case_numbers)
-    print("Remaining Values:\n", case_values)
-    print("\nBased on what the banker has seen they are willing to offer you $" + str(offer) + " at this time to walk away!\n")
-    print("Would you like to take this deal?")
-    response = input("Y or N: ").upper()
-    if response == "Y":
-        print("\nCongrats you have won $" + str(offer))
-        print("\nThanks for playing!")
-    elif response == "N":
-        print("\nAlright then the game continues!\n")
-    else:
-        print("\nPlease answer with a Y or N")
-        banker_process(case_values)
-    return response
-    
+            
 personal_case_process(personal_case_number)
 
 game_play(case_values, countdown)
-
